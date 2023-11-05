231b90e7d41fac40a844bc88f80556c171d89dab
razvanghr <razvan_alin02@yahoo.com>
2023-11-06 00:55:11 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import gif from "./assets/astro_gif.gif";
-import gif2 from "./assets/computer_gif.gif";
-
 function App() {
-  return (
-    <div className="App">
-      <h1>SMECHER DE MIC MA FACUT MAMA</h1>
-      <img src={gif} className="gif"></img>
-      <img src={gif2} className="gif2"></img>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
